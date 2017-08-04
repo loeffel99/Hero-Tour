ce97729ef7e43ba7a03516519931bfdedf25d387
Tim Löffelholz <timloe@Tims-MacBook-Pro.local>
2017-08-04 12:27:06 +0200

    Complete the tour of heroes tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,39 @@
-import { NgModule }      from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { HttpModule } from '@angular/http';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
